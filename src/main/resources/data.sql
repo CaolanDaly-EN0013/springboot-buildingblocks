1de37bfd6b97b00177cb315d9d2d092e38410d2d
CaolanDaly-EN0013 <cdaly@aflac.com>
2022-01-17 14:38:45 +0000

    Added JPA base REST services

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER VALUES(101, 'caolan.daly@email.com', 'Caolán', 'Daly', 'admin', 'SSN101', 'cdaly101');
+INSERT INTO USER VALUES(102, 'ginara.cruz@email.com', 'Ginara', 'Cruz', 'admin', 'SSN102', 'cdaly102');
+INSERT INTO USER VALUES(103, 'john.smith@email.com', 'John', 'Smith', 'admin', 'SSN103', 'cdaly103');
