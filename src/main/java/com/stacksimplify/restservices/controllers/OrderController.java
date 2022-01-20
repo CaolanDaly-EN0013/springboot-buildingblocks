e7fff36bc2a21879bb38f205361f09ca3ad210b4
CaolanDaly-EN0013 <cdaly@aflac.com>
2022-01-20 13:27:01 +0000

    Added one to many functionality

---
+++ src/main/java/com/stacksimplify/restservices/controllers/OrderController.java
@@ -0,0 +1,67 @@
+package com.stacksimplify.restservices.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.constraints.Min;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stacksimplify.restservices.Exceptions.OrderNotFoundException;
+import com.stacksimplify.restservices.Exceptions.UserNotFoundException;
+import com.stacksimplify.restservices.entities.Order;
+import com.stacksimplify.restservices.entities.User;
+import com.stacksimplify.restservices.repositories.OrderRepository;
+import com.stacksimplify.restservices.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class OrderController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@GetMapping("/{userId}/orders")
+	public List<Order>getAllOrders(@PathVariable Long userId) throws UserNotFoundException {
+		Optional<User> optionalUser = userRepository.findById(userId);
+		
+		if(!optionalUser.isPresent()) {
+			throw new UserNotFoundException("User not found.");
+		}
+		
+		return optionalUser.get().getOrders();
+	}
+	
+	@PostMapping("{userId}/orders")
+	public Order createOrder(@PathVariable Long userId, @RequestBody Order order) throws UserNotFoundException {
+		Optional<User> optionalUser = userRepository.findById(userId);
+		
+		if(!optionalUser.isPresent()) {
+			throw new UserNotFoundException("User not found.");
+		}
+		
+		User user = optionalUser.get();
+		order.setUser(user);
+		return orderRepository.save(order);
+	}
+	
+	@GetMapping("/{userId}/orders/{orderId}")
+	public Optional<Order> getOrderById(@PathVariable Long userId, @PathVariable Long orderId) throws OrderNotFoundException {
+		Optional<Order> order = orderRepository.findById(orderId);
+		
+		if(!order.isPresent()) {
+			throw new OrderNotFoundException("Order not found in repository.");
+		}
+				
+		return order;
+	}
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
 INSERT INTO USER VALUES(101, 'caolan.daly@email.com', 'Caolán', 'Daly', 'admin', 'SSN101', 'cdaly101');
 INSERT INTO USER VALUES(102, 'ginara.cruz@email.com', 'Ginara', 'Cruz', 'admin', 'SSN102', 'cdaly102');
 INSERT INTO USER VALUES(103, 'john.smith@email.com', 'John', 'Smith', 'admin', 'SSN103', 'cdaly103');
+INSERT INTO ORDERS VALUES(2001, 'order1', 101);
+INSERT INTO ORDERS VALUES(2002, 'order2', 101);
+INSERT INTO ORDERS VALUES(2003, 'order3', 101);
+INSERT INTO ORDERS VALUES(2004, 'order4', 102);
+INSERT INTO ORDERS VALUES(2005, 'order5', 102);
+INSERT INTO ORDERS VALUES(2006, 'order6', 103);
