661a2b8f3c97376d9b5f16062a2a5a19d713efee
CaolanDaly-EN0013 <cdaly@aflac.com>
2022-02-14 09:22:05 +0000

    ModelMapper first commit

---
+++ src/main/java/com/stacksimplify/restservices/dto/UserMmDto.java
@@ -0,0 +1,31 @@
+package com.stacksimplify.restservices.dto;
+
+public class UserMmDto {
+	
+	private Long userId;
+	private String username;
+	private String firstname;
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+		
+	
+	
+
+}
